833efa79a3e627c703eaac336999ed1c30d53d43
Samar <bantmasr21@gmail.com>
2024-06-10 16:28:18 -0700

    api/v1/app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,6 +4,7 @@ Definition of the API server
 """
 
 from flask import Flask
+from flask import jsonify
 from models import storage
 from api.v1.views import app_views
 from flask_cors import CORS
@@ -17,6 +18,12 @@ app.register_blueprint(app_views)
 @app.teardown_appcontext
 def teardown(exception):
     storage.close()
+
+
+@app.errorhandler(404)
+def not_found(error):
+    """Handler for 404 errors that returns a JSON response"""
+    return jsonify({"error": "Not found"}), 404
 
 
 if __name__ == "__main__":
