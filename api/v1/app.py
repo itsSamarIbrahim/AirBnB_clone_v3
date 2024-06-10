8a790f4435a3f51dec05e1522c00fe80532f513f
Samar <bantmasr21@gmail.com>
2024-06-10 16:19:25 -0700

    fixed for task 3

--- api/v1/app.py
+++ api/v1/app.py
@@ -13,12 +13,13 @@ app = Flask(__name__)
 CORS(app)  # Enable CORS for the entire app
 app.register_blueprint(app_views)
 
+
 @app.teardown_appcontext
 def teardown(exception):
     storage.close()
+
 
 if __name__ == "__main__":
     host = os.getenv('HBNB_API_HOST', '0.0.0.0')
     port = os.getenv('HBNB_API_PORT', '5000')
     app.run(host=host, port=port, threaded=True)
-
