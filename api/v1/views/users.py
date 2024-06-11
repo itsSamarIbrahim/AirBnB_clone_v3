f8ea011b3aaf9e38350a6d0d37100441cedac945
Samar <bantmasr21@gmail.com>
2024-06-10 19:53:03 -0700

    updated

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -8,11 +8,13 @@ from api.v1.views import app_views
 from models import storage
 from models.user import User
 
+
 @app_views.route('/users', methods=['GET'], strict_slashes=False)
 def get_users():
     """Retrieves the list of all User objects"""
     users = [user.to_dict() for user in storage.all(User).values()]
     return jsonify(users)
+
 
 @app_views.route('/users/<user_id>', methods=['GET'], strict_slashes=False)
 def get_user(user_id):
@@ -21,6 +23,7 @@ def get_user(user_id):
     if not user:
         abort(404)
     return jsonify(user.to_dict())
+
 
 @app_views.route('/users/<user_id>', methods=['DELETE'], strict_slashes=False)
 def delete_user(user_id):
@@ -31,6 +34,7 @@ def delete_user(user_id):
     storage.delete(user)
     storage.save()
     return jsonify({}), 200
+
 
 @app_views.route('/users', methods=['POST'], strict_slashes=False)
 def create_user():
@@ -46,6 +50,7 @@ def create_user():
     storage.new(new_user)
     storage.save()
     return jsonify(new_user.to_dict()), 201
+
 
 @app_views.route('/users/<user_id>', methods=['PUT'], strict_slashes=False)
 def update_user(user_id):
